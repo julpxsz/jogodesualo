40ac3eede1e4a025c2c3d63ebdb4bd16d01352d1
julpxsz <jutrindade603@gmail.com>
2025-03-10 15:30:21 -0300

    fim!!!!!

---
+++ trabalhodesualo4b/Assets/script/AnimacaoDoJogador.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimacaoDoJogador : MonoBehaviour
+{
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private Rigidbody2D rigidbody;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float velocidadeX = Mathf.Abs(this.rigidbody.velocity.x);
+        if (velocidadeX > 0)
+        {
+            this.animator.SetBool( "Andando",true);
+        }
+        else
+        {
+            this.animator.SetBool("Andando",false);
+        }
+    }
+}
